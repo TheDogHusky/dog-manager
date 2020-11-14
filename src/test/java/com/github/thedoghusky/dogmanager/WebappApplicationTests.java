27a040aebc101c40f7e99be849d7763ec093886a
BuildTools <unconfigured@null.spigotmc.org>
2020-11-14 18:49:38 +0100

    Introducing Spring MVC

---
+++ src/test/java/com/github/thedoghusky/dogmanager/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thedoghusky.dogmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
