91c1190d7855d08b4b024d102f36da71340e80bc
BuildTools <unconfigured@null.spigotmc.org>
2020-11-18 18:52:47 +0100

    WOUF

--- src/main/java/com/github/thedoghusky/dogmanager/DogsController.java
+++ src/main/java/com/github/thedoghusky/dogmanager/DogsController.java
@@ -12,10 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DogsController {
 	
 	
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
+	@GetMapping("/")
+	public String homePage(Model model) {
+		return "home";
 	}
 	
 	@GetMapping("/dogs")
