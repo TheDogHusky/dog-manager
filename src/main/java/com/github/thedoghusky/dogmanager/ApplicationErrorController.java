48e0205b6937dc360ace79b27db14603e10afd15
ClassyCrafter <classy.craft3r@gmail.com>
2020-11-25 20:57:11 +0100

    Ajout d'une page 404
    Closes #3

---
+++ src/main/java/com/github/thedoghusky/dogmanager/ApplicationErrorController.java
@@ -0,0 +1,32 @@
+package com.github.thedoghusky.dogmanager;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+	public class ApplicationErrorController implements ErrorController  {
+	 
+	    @RequestMapping("/error")
+	    public String handleError(HttpServletRequest request) {
+	    	Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		    
+		    if (status != null) {
+		        Integer statusCode = Integer.valueOf(status.toString());
+		    
+		        if(statusCode == HttpStatus.NOT_FOUND.value()) {
+		            return "404";
+		        }
+		    }
+		    return "error";
+	    }
+	 
+	    @Override
+	    public String getErrorPath() {
+	        return null;
+	    }
+	}
